a0f8ecde98e8a7a36b3ad251b1a1ab40f2fc6385
Techdoodle <chris@techchrism.me>
2021-01-03 12:12:52 -0800

    remove text asset webpack and add wii oui-list

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
     'transpileDependencies': [
         'vuetify'
-    ],
-    chainWebpack: config => {
-        config.module
-               .rule('raw')
-               .test(/\.txt$/)
-               .use('raw-loader')
-               .loader('raw-loader')
-               .end()
-    }
+    ]
 };
