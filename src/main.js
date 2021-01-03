02bb23392660dfa8135402e115109344d7de004a
Techdoodle <chris@techchrism.me>
2021-01-03 11:41:38 -0800

    format code and remove hello-world

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+    vuetify,
+    render: function(h)
+    {
+        return h(App);
+    }
+}).$mount('#app');
