c9c85b298546cbf77cec201ca5378a2f19676a34
techchrism <chris@techchrism.me>
2023-05-15 17:31:46 -0700

    implement mac input component

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import type { Component } from 'solid-js';
+import type {Component} from 'solid-js';
+import letterbombIcon from './assets/letterbomb_icon.png'
+import MacInput from './components/MacInput'
 
 const App: Component = () => {
-  return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-  );
+    const onMacInput = (mac: string, valid: boolean) => {
+        console.log(mac, valid)
+    }
+
+    return (
+        <>
+            <main class="flex flex-col mt-4 space-y-5 text-center">
+                <div>
+                    <img class="inline object-contain flex-shrink" src={letterbombIcon} alt="letterbomb icon"/>
+
+                    <h1 class="text-4xl font-semibold">Letterbomb Web</h1>
+                    <h2 class="text-2xl">Clientside version of Letterbomb which is statically hosted</h2>
+                </div>
+                <div>
+                    <MacInput onMacChange={onMacInput}/>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default App;
